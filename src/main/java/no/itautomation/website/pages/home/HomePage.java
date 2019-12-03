eb6a40f034f80c759c5d1aa8fe8c01647f28875f
senth <senth@host.docker.internal>
2019-12-03 09:53:05 +0100

    updated xpath

--- src/main/java/no/itautomation/website/pages/home/HomePage.java
+++ src/main/java/no/itautomation/website/pages/home/HomePage.java
@@ -8,7 +8,7 @@ import no.itautomation.website.hooks.ItAutomationDriver;
 
 public class HomePage {
 	
-    public String HOME_TEXT = "//*[@id='post-10']/div/div/div/div/section[1]/div[3]/div/div[1]/div/div/div[1]/div/div";
+    public String HOME_TEXT = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/main[1]/article[1]/div[1]/div[1]/div[1]/div[1]/section[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/p[1]";
     public String CONTACT_MENU = "//a[contains(text(),'Contact')]";
     
 	protected ItAutomationDriver driver;
